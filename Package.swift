bc987e415444d1d8dd487f282437f9eb51aa0dfc
Brooks Cutter <brooksc@brooksc.com>
2026-04-29 22:15:57 -0700

    Use brooksc/MisakiSwift fork with mlx-swift 0.31.3+ support

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.31.3")),
     // .package(url: "https://github.com/mlalma/eSpeakNGSwift", from: "1.0.1"),
-    .package(url: "https://github.com/mlalma/MisakiSwift", exact: "1.0.6"),
+    .package(url: "https://github.com/brooksc/MisakiSwift", branch: "main"),
     .package(url: "https://github.com/mlalma/MLXUtilsLibrary.git", from: "0.0.6")
   ],
   targets: [
